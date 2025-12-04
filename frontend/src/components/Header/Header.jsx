df3357f265535230eced70f801bf15664b92710f
Amit000100 <amitrajusharma5@.com>
2025-12-05 00:42:05 +0530

    add some files

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='Header'>
+        <div className="header-content">
+            <h2>Order your favourite food here</h2>
+            <p>choose from a diverse menu featuring a delectable arry of dish crafted with finest ingrediates and culinary expertise. Our mission is to satisfy your caravings elevate your dining experience, one delicious meal at time.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
