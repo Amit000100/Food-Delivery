f2f87c976db80c3827e32684cafa1953a6c54909
Amit000100 <amitrajusharma5@gmail.com>
2025-12-05 23:38:42 +0530

    add component

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div className='Header'>
+    <div className='header'>
         <div className="header-content">
             <h2>Order your favourite food here</h2>
             <p>choose from a diverse menu featuring a delectable arry of dish crafted with finest ingrediates and culinary expertise. Our mission is to satisfy your caravings elevate your dining experience, one delicious meal at time.</p>
