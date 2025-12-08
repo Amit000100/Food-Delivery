6c635c89361bffc914f4925a63efbc3f3f10bdf7
Amit000100 <amitrajusharma5@gmail.com>
2025-12-09 01:30:12 +0530

    other components

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
+import { StoreContext } from '../../context/StoreContext'
 
 const Cart = () => {
+
+  const {cartItems, food_list, removeFromCart} = useContext(StoreContext);
+
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <div className="cart-item">
+        <div className="cart-item-title">
+          <p>Item</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item,index)=>{
+          if(cartItems[item._id] > 0){
+
+            return (
+              <div  className="cart-item-title cart-items-item">
+                <img src={item.image} alt="" />
+                <p>{item.name}</p>
+                <p>${item.price}</p>
+                <p>{cartItems[item._id]}</p>
+                <p>${(item.price * cartItems[item._id]).toFixed(2)}</p>
+                <button onClick={()=>removeFromCart(item._id)}>Remove</button>
+              </div>
+                )
+            }
+        })}
+      </div>
+    </div>
   )
 }
 
